a86f9faf8193b1c92d1f64188feb4a30ec3266dc
phamduy <dreamow9@gmail.com>
2026-03-08 16:56:11 +0700

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   site: 'https://dreamow99.github.io/chuachien-wiki',
-  base: '/chuachien-wiki/',
+  base: '/chuachien-wiki',
 });
